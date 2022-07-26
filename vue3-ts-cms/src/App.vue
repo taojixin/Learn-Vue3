ff7e6c91e01a06dae67e6d7a2ac1be5b392d28f0
Tao <491675919@qq.com>
2022-07-26 20:04:24 +0800

    2022.7.16项目创建

---
+++ vue3-ts-cms/src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div id="nav">
+    <router-link to="/login">login</router-link> |
+    <router-link to="/main">main</router-link>
+  </div>
+  <router-view />
+
+  <el-button>按钮</el-button>
+</template>
+
+<style lang="less"></style>
---
+++ vue3-ts-cms/src/router/index.ts
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    component: () => import('@/views/login/login.vue')
+  },
+  {
+    path: '/main',
+    component: () => import('@/views/main/main.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
