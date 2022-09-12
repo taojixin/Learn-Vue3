dd165f03b5daf85a510d242595cfd7923a2ed438
Tao <491675919@qq.com>
2022-09-12 20:44:56 +0800

    xuran

--- hy-trip/src/views/home/cpns/home-content.vue
+++ hy-trip/src/views/home/cpns/home-content.vue
@@ -3,13 +3,22 @@
     <h2 class="title">热门精选</h2>
     <div class="list">
       <template v-for="item in houselist" :key="item.data.houseId">
-        <h3>{{ item.data.houseName }}</h3>
+        <house-item-v9
+          v-if="item.discoveryContentType === 9"
+          :item-data="item.data"
+        />
+        <house-item-v3
+          v-else-if="item.discoveryContentType === 3"
+          :item-data="item.data"
+        />
       </template>
     </div>
   </div>
 </template>
 
 <script setup>
+import HouseItemV9 from "@/components/house-item-v9/house-item-v9.vue";
+import HouseItemV3 from "@/components/house-item-v3/house-item-v3.vue";
 import useHomeStore from "@/stores/modules/home";
 import { storeToRefs } from "pinia";
 
@@ -23,6 +32,10 @@ const { houselist } = storeToRefs(homeStore);
   .title {
     font-size: 12px;
     padding: 10px;
+  }
+  .list {
+    display: flex;
+    flex-wrap: wrap;
   }
 }
 </style>
