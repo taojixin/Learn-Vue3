a9319f191b3c755aa5f8e5ab4e737dcd9be853cf
taojixin <491675919@qq.com>
2022-11-09 22:40:05 +0800

    a

---
+++ vue3-music-app/src/components/music-list/music-list.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="music-list">
+    <div class="back">
+      <i class="icon-back"></i>
+    </div>
+    <h1 class="title">{{ title }}</h1>
+    <div class="bg-image" :style="bgImageStyle">
+      <div class="filter"></div>
+    </div>
+    <!-- <scroll class="list">
+      <div class="song-list-wrapper">
+        <song-list :songs="songs"></song-list>
+      </div>
+    </scroll> -->
+  </div>
+</template>
+
+<script setup>
+import { computed } from "@vue/reactivity";
+import { defineProps } from "vue";
+const props = defineProps({
+  songs: {
+    type: Array,
+    default() {
+      return [];
+    },
+  },
+  title: String,
+  pic: String,
+});
+
+const bgImageStyle = computed(() => {
+  return {
+    backgroundImage: `url(${props.pic})`,
+  };
+});
+</script>
+
+<style lang="scss" scoped>
+.music-list {
+  position: relative;
+  height: 100%;
+  .back {
+    position: absolute;
+    top: 0;
+    left: 6px;
+    z-index: 20;
+    transform: translateZ(2px);
+    .icon-back {
+      display: block;
+      padding: 10px;
+      font-size: $font-size-large-x;
+      color: $color-theme;
+    }
+  }
+  .title {
+    position: absolute;
+    top: 0;
+    left: 10%;
+    width: 80%;
+    z-index: 20;
+    transform: translateZ(2px);
+    @include no-wrap();
+    text-align: center;
+    line-height: 40px;
+    font-size: $font-size-large;
+    color: $color-text;
+  }
+  .bg-image {
+    position: relative;
+    width: 100%;
+    transform-origin: top;
+    background-size: cover;
+    .play-btn-wrapper {
+      position: absolute;
+      bottom: 20px;
+      z-index: 10;
+      width: 100%;
+      .play-btn {
+        box-sizing: border-box;
+        width: 135px;
+        padding: 7px 0;
+        margin: 0 auto;
+        text-align: center;
+        border: 1px solid $color-theme;
+        color: $color-theme;
+        border-radius: 100px;
+        font-size: 0;
+      }
+      .icon-play {
+        display: inline-block;
+        vertical-align: middle;
+        margin-right: 6px;
+        font-size: $font-size-medium-x;
+      }
+      .text {
+        display: inline-block;
+        vertical-align: middle;
+        font-size: $font-size-small;
+      }
+    }
+    .filter {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: rgba(7, 17, 27, 0.4);
+    }
+  }
+  .list {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    z-index: 0;
+    .song-list-wrapper {
+      padding: 20px 30px;
+      background: $color-background;
+    }
+  }
+}
+</style>
--- vue3-music-app/src/views/singer-detail.vue
+++ vue3-music-app/src/views/singer-detail.vue
@@ -1,21 +1,45 @@
 <template>
-  <div class="singer-detail"></div>
+  <div class="singer-detail">
+    <MusicList :songs="songs" :title="title" :pic="pic"></MusicList>
+  </div>
 </template>
 
 <script setup>
 import { getSingerDetail } from "@/service/singer";
 import { processSongs } from "@/service/song";
+import MusicList from "@/components/music-list/music-list.vue";
 
-import { defineProps, onMounted } from "vue";
+import { defineProps, onMounted, ref } from "vue";
+import { computed } from "@vue/reactivity";
 const props = defineProps({
   singer: Object,
 });
 
+let songs = ref([]);
+const pic = computed(() => {
+  return props.singer && props.singer.pic;
+});
+const title = computed(() => {
+  return props.singer && props.singer.name;
+});
+
 onMounted(async () => {
   const result = await getSingerDetail(props.singer);
-  const songs = await processSongs(result.songs);
-  console.log(songs);
+  console.log(result);
+  //   const songs = await processSongs(result.songs);
+  //   console.log(songs);
+  songs.value = result.songs;
 });
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.singer-detail {
+  position: fixed;
+  z-index: 10;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  background: $color-background;
+}
+</style>
